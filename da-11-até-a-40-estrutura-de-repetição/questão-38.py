1b768289b852a179113324d8fb4072f113e54886
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2016-09-04 22:03:08 -0300

    ,

--- da-11-até-a-40-estrutura-de-repetição/questão-38.py
+++ da-11-até-a-40-estrutura-de-repetição/questão-38.py
@@ -1,9 +1,9 @@
 salario=float(input("informe seu salario"))
 somador=1.5/100
 anoprimeiro=1995
-while 1995>2016:
+while anoprimeiro>2016:
 	anoprimeiro=anoprimeiro+1
-	salario=(salario*0.15) + salario
+	salario=(salario*somador) + salario
 	while ano>=1997 and ano<2016:
  		somador=somador*2
 print(salario*somador)
