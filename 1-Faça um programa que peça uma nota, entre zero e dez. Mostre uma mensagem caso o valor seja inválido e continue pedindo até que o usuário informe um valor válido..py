a3cd59951282e70567b228424f8c28188f158203
MatheusCarvalho-dev <79205397+MatheusCarvalho-dev@users.noreply.github.com>
2021-10-18 12:46:48 -0300

    Reajuste no código
    O código antigo retornava ao loop se o número fosse menor que 0 e maior que 10, o contrário do exercício proposto

--- 1-Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido..py
+++ 1-Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido..py
@@ -3,3 +3,11 @@ nota=float(input("informe um numero de 0 a 10: "))
 while (nota>10) or (nota<0):
 	nota=float(input("informe um numero de 0 a 10: "))
 
+#sugestao
+nota = int(input('Digite uma nota entre 0 e 10 '))
+
+while (nota > 0) and (nota < 10): 
+    nota = int(input('Digite uma nota entre 0 e 10: ')) 
+else:
+    print('Valor inválido')
+    exit()
